096c722a2f36389ad585ce65452e7157d7294a43
Alejandro Cortell Marín <alejandrocortellmarin@gmail.com>
2022-09-29 20:36:07 +0200

    getSum

---
+++ getSum/getSum.js
@@ -0,0 +1,27 @@
+/*
+Given two integers a and b, which can be positive or negative, find the sum of all 
+the integers between and including them and return it. If the two numbers are equal 
+return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+*/
+
+function getSum(a, b) {
+  const lower = a < b ? a : b
+  const higher = a > b ? a : b
+  const numbers = []
+  for (let x = lower; x <= higher; x++) {
+    numbers.push(x)
+  }
+
+  return numbers.reduce((prev, curr) => prev + curr, 0)
+}
